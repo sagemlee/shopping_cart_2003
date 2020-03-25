2f2d966ef6220a75094b4e0ed1864941430e06be
Sage Lee <sagemcklee@gmail.com>
2020-03-25 09:13:26 -0600

    Add product class and test

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,12 @@
-class Prodct
+class Product
+    attr_reader :category, :name, :quantity
+    attr_accessor :unit_price
 
-  def initialize(category, name, unit_price, quantity)
+  def initialize(category, name, unit_price = 3.70, quantity)
     @category = category
-    @name = name_parameter
-    @unit_price = 3.70
-    @quantity = quantity
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
   end
 
   def total_price
